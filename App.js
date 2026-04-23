15ba3560b53cef1b0df0be7928acf013ce57b9b0
GoBrazill <vinicius.daraujos26@gmail.com>
2026-04-23 11:08:20 -0300

    feat: render api consuptiom in app

--- App.js
+++ App.js
@@ -1,20 +1,112 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState, useEffect } from "react";
+import { View, Text, Image, ScrollView, TextInput } from "react-native";
+import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
+
+import api from './src/services/api'
+import { globalStyles } from "./src/styles/globalStyles";
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+  const [filmes, setFilmes] = useState([])
+  const [pesquisa, setPesquisa] = useState('')
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+  useEffect(() => {
+    async function carregarFilmes() {
+      if (pesquisa.trim() !== '') {
+        try {
+          const res = await api.get(pesquisa.replace('', '%20'))
+          setFilmes(res.data)
+        } catch (err) {
+          console.error('erro na requisição: ', err)
+        }
+      } else {
+        setFilmes([])
+      }
+    }
+    carregarFilmes()
+  }, [pesquisa])
+
+  const handlePesquisa = (texto) => {
+    setPesquisa(texto)
+  }
+
+  return (
+    <SafeAreaProvider>
+      <SafeAreaView
+        style={globalStyles.container}>
+        <TextInput
+          style={globalStyles.input}
+          placeholder="digite o nome do filme"
+          value={pesquisa}
+          onChangeText={handlePesquisa}
+        />
+        <Text
+          style={globalStyles.titulo}
+        > Resultado(s) da pesquisa
+        </Text>
+
+        <ScrollView>
+          {filmes.map((filme) => (
+            <View
+              key={filme.show.id}
+              style={globalStyles.card}
+            >{filme.show.image && (
+              <Image
+                source={{ uri: filme.show.image.medium }}
+                style={globalStyles.image}
+                resizeMode='cover'
+              />
+            )}
+
+              <View
+                style={globalStyles.infoContainer}>
+                <Text
+                  style={globalStyles.tituloFilme}>
+                  {filme.show.name}
+                </Text>
+                <Text style={globalStyles.url}>
+                  {filme.show.url}
+                </Text>
+              </View>
+
+            </View>
+          ))}
+        </ScrollView>
+      </SafeAreaView>
+    </SafeAreaProvider>
+  )
+
+  // return(
+  //   <SafeAreaView style={globalStyles.container}>
+  //     <TextInput style={globalStyles.input}
+  //       placeholder="Digite o nome do filme"
+  //       value={pesquisa}
+  //       onChangeText={handlePesquisa}
+  //     />
+  //     <Text style={globalStyles.titulo}>Resultados(s) da pesquisa</Text>
+
+  //     <ScrollView>
+  //       {filmes.map((filme) => {
+  //         <View key={filme.show.id style={globalStyles.card>
+  //         {
+  //           filme.show.image && (
+  //             <Image
+  //               source={{ uri: filme.show.image.medium }}
+  //               style={globalStyles.imagem}
+  //               resizeMode='cover'
+  //             />
+  //           )
+  //         }
+  //           <View style={globalStyles.infoContainer}>
+  //             <Text style={globalStyles.tituloFilme}>
+  //               {filme.show.name}
+  //             </Text>
+  //             <Text style={globalStyles.url}>
+  //             {filme.show.url}
+  //             </Text>
+  //           </View>
+  //         </View>
+  //       })}
+  //     </ScrollView>
+  //   </SafeAreaView>
+  // )
+}
